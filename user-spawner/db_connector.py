04172ab2defc966c2de22ef4b5eb1c9d728be86f
astrowicked@gmail.com <astrowicked@gmail.com>
2014-11-03 18:22:13 -0600

    Added a cassandra connection w/o auth and made pep8 changes.

--- user-spawner/db_connector.py
+++ user-spawner/db_connector.py
@@ -2,9 +2,18 @@
 
 from pymongo import MongoClient
 from redis import Redis
+from cassandra.cluster import Cluster
+
 
 class DBConnector(object):
-    def __init__(self, hostname=None, port=None, authdb=None, username=None, password=None):
+    def __init__(
+        self,
+        hostname=None,
+        port=None,
+        authdb=None,
+        username=None,
+        password=None
+    ):
         self.hostname = hostname
         self.port = port
         self.username = username
@@ -12,25 +21,35 @@ class DBConnector(object):
         self.authdb = authdb
 
     def _check_attributes(self, exclude_list=list()):
-        for attrib in ['hostname','port','authdb','username','password']:
-            if getattr(self,attrib) is None and attrib not in exclude_list:
+        attrib_list = ['hostname', 'port', 'authdb', 'username', 'password']
+        for attrib in attrib_list:
+            if getattr(self, attrib) is None and attrib not in exclude_list:
+                answer = raw_input('Please provide me a {}: '.format(attrib))
                 if attrib is not 'port':
-                    setattr(self,attrib,raw_input('Please provide me a {}: '.format(attrib)))
-                    print getattr(self,attrib)
+                    setattr(self, attrib, answer)
+                    print getattr(self, attrib)
                 else:
-                    setattr(self,attrib,int(raw_input('Please provide me a {}: '.format(attrib))))
-                    print getattr(self,attrib)
+                    setattr(self, attrib, int(answer))
+                    print getattr(self, attrib)
 
-
+    def cassandra_connection(self):
+        self._check_attributes(['authdb', 'username', 'password'])
+        connection = Cluster([self.hostname], port=self.port).connect()
+        return connection
 
     def mongo_connection(self):
         self._check_attributes()
         connection = MongoClient(self.hostname, self.port)
-        connection[self.authdb].authenticate(self.username,self.password)
+        connection[self.authdb].authenticate(self.username, self.password)
         db = connection['users']
         return db
-        
+
     def redis_connection(self):
-        self._check_attributes(['authdb','username'])
-        connection = Redis(host=self.hostname,port=self.port,db=self.authdb,password=self.password)
+        self._check_attributes(['authdb', 'username'])
+        connection = Redis(
+            host=self.hostname,
+            port=self.port,
+            db=self.authdb,
+            password=self.password
+        )
         return connection
